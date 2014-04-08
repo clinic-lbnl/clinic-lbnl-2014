d6ee001d8d606f0b6b7bede76da1d43ef158019b
Spencer Harris <slharris@cheery.math.hmc.edu>
2014-04-07 19:43:40 -0700

    No longer need to hard-code files in reducer.

--- disease-comparison/src/main/java/disease_comparison/GraphTesting.java
+++ disease-comparison/src/main/java/disease_comparison/GraphTesting.java
@@ -1,16 +1,62 @@
 package disease_comparison;
 
+import java.util.Random;
+import java.util.Set;
+
 public class GraphTesting {
+	
+	/*
+	public static String gradient(Ontology o) {
+		
+		String cur = o.root;
+		Random r = new Random();
+		
+		String ans = "";
+		
+		while (true)
+		{
+			ans += o.node_map.get(cur).getICScore() + ", ";
+			
+			Set<String> children = o.children(cur);
+			if (children.isEmpty())
+			{
+				break;
+			}
+			
+			int index = r.nextInt(children.size());
+			int i = 0;
+			
+			for (String child : children)
+			{
+				if (i == index)
+				{
+					cur = child;
+					break;
+				}
+				i++;
+			}
+		}
+				
+		return ans;
+	}
+	*/
 	
 	public static void main(String [] args) {
 		
-		Ontology o = new Ontology("../../test-files/class-labels.txt", "../../test-files/class-to-class.txt",
-				"../../test-files/individual-labels.txt", "../../test-files/individual-to-class.txt");
+		Ontology o = new Ontology("../../test-files/small-class-labels.txt", "../../test-files/small-class-to-class.txt",
+				"../../test-files/small-individual-labels.txt", "../../test-files/small-individual-to-class.txt");
 		
-		OutputOptions options = new OutputOptions();
+		long start_time = System.nanoTime();
+		try
+		{
+			o.compareAllDiseases();
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.toString());
+		}
 		
-		o.compareAllDiseases("./out.txt", options);
-		
-		//System.out.println(o.root());
+		long end_time = System.nanoTime();
+		System.out.println("Comparing diseases took " + (end_time - start_time) / 1000 + " microseconds");
 	}
 }
