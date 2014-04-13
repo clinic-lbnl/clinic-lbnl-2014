bf43922b2c032652b9c1c86e3e9573a2e68f2c84
Spencer Harris <slharris@cheery.math.hmc.edu>
2014-04-13 13:53:32 -0700

    Added command line input, fixed spelling.

--- disease-comparison/run.sh
+++ disease-comparison/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 bash clean.sh
-mvn exec:java -Dexec.mainClass=disease_comparison.DiseaseComparisonDriver
+mvn exec:java -Dexec.mainClass=disease_comparison.DiseaseComparisonDriver -Dexec.args="../../test-files/small-class-labels.txt ../../test-files/small-class-to-class.txt ../../test-files/small-individual-labels.txt ../../test-files/small-individual-to-class.txt"
